c625dfe0e8e66dbd4f4cbac7191046a054ae83e5
Gabriel Brizuela <fgb.8513@gmail.com>
2022-09-06 19:24:37 -0300

    ultimo arreglo

--- src/Components/Item.jsx
+++ src/Components/Item.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-const Item = ({nombre, precio, image}) => {
+const Item = ({nombre, image, precio}) => {
     return (
         <div className="card">
             <img src={image} className="card-img-top" alt={nombre} />
             <div className="card-body">
                 <h5 className="card-title text-center"><b>{nombre}</b></h5>
-                <p className="card-text text-center">${precio}</p>
+                <h4 className="card-text text-center">${precio}</h4>
             </div>
         </div>
     )
--- src/Components/ItemListContainer.jsx
+++ src/Components/ItemListContainer.jsx
@@ -8,11 +8,11 @@ const ItemListContainer = () => {
 
     useEffect (()=> {
         const productos = [
-            { "id": 1, "nombre": "Bidon 20 lts", "precio": 550, "descripcion": "Bidón retornable de agua x 20 litros", "image": "../img/Bidon-retornable-de-agua-x-20-litros.jpg" },
-            { "id": 2, "nombre": "Bidon 10 lts", "precio": 300, "descripcion": "Bidón retornable de agua x 10 litros", "image": "https://sodayaguastrubolini.com.ar/wp-content/uploads/2017/04/Bidon-retornable-de-agua-x-10-litros.jpg" },
-            { "id": 3, "nombre": "Bidon 8 lts", "precio": 250, "descripcion": "Bidón descartable de agua x 8 litros", "image": "https://sodayaguastrubolini.com.ar/wp-content/uploads/2017/04/Bidon-descartable-de-agua-x-8-litros.jpg" },
-            { "id": 4, "nombre": "Sifon plastico", "precio": 100, "descripcion": "Sifón plástico retornable de soda x 1,25 litros", "image": "https://sodayaguastrubolini.com.ar/wp-content/uploads/2017/04/Sifon-plastico-de-soda-x-1-25-litros.jpg"},
-            { "id": 5, "nombre": "Sifon vidrio", "precio": 90, "descripcion": "Sifón vidrio retornable de soda x 1 litro", "image": "https://sodayaguastrubolini.com.ar/wp-content/uploads/2017/04/Sifon-de-vidrio-de-soda-x-1-litro.jpg" },
+            { "id": 1, "nombre": "Bidon 20 lts", "precio":550, "descripcion": "Bidón retornable de agua x 20 litros", "image": "https://sodayaguastrubolini.com.ar/wp-content/uploads/2017/04/Bidon-retornable-de-agua-x-20-litros.jpg" },
+            { "id": 2, "nombre": "Bidon 10 lts", "precio":300, "descripcion": "Bidón retornable de agua x 10 litros", "image": "https://sodayaguastrubolini.com.ar/wp-content/uploads/2017/04/Bidon-retornable-de-agua-x-10-litros.jpg" },
+            { "id": 3, "nombre": "Bidon 8 lts", "precio":250, "descripcion": "Bidón descartable de agua x 8 litros", "image": "https://sodayaguastrubolini.com.ar/wp-content/uploads/2017/04/Bidon-descartable-de-agua-x-8-litros.jpg" },
+            { "id": 4, "nombre": "Sifon plastico", "precio":100, "descripcion": "Sifón plástico retornable de soda x 1,25 litros", "image": "https://sodayaguastrubolini.com.ar/wp-content/uploads/2017/04/Sifon-plastico-de-soda-x-1-25-litros.jpg"},
+            { "id": 5, "nombre": "Sifon vidrio", "precio":90, "descripcion": "Sifón vidrio retornable de soda x 1 litro", "image": "https://sodayaguastrubolini.com.ar/wp-content/uploads/2017/04/Sifon-de-vidrio-de-soda-x-1-litro.jpg" },
         ];
        setItems(productos);
 
